a33948a17d66b0c7b33012631f67c082491bfe9f
Will Nichols <william.i.nichols@gmail.com>
2021-08-24 22:00:57 -0400

    first working version

--- scripts/target-lock.js
+++ scripts/target-lock.js
@@ -1,6 +1,130 @@
-console.log('target-lock | Hello World!');
+console.log("target-lock | Hello World!");
 
-Hooks.on('renderChatMessage', async (message, html, data) => {
+async function applyActorDamage(
+    roll,
+    tokenID,
+    multiplier,
+    attribute = 'attributes.hp',
+    modifier = 0,
+    shieldID = {}
+) {
+    const tokens = canvas.tokens.ownedTokens.filter((token) => token.data._id === tokenID && token.actor);
+    if (tokens.length === 0) {
+        ui.notifications.error(game.i18n.localize('PF2E.UI.errorTargetToken'));
+        return false;
+    }
+
+    const value = Math.floor(parseFloat(roll.find('.dice-total').text()) * multiplier) + modifier;
+    const messageSender = roll.find('.message-sender').text();
+    const flavorText = roll.find('.flavor-text').text();
+    for (const token of tokens) {
+        const actor = token.actor;
+        const shield =
+            attribute === 'attributes.shield'
+                ? shieldID
+                    ? actor.itemTypes.armor.find((armor) => armor.isShield && armor.id === shieldID) ?? null
+                    : actor.heldShield
+                : null;
+        if (attribute === 'attributes.shield' && shield?.isBroken) {
+            const warnings = LocalizePF2e.translations.PF2E.Actions.RaiseAShield;
+            ui.notifications.warn(
+                game.i18n.format(warnings.ShieldIsBroken, { actor: token.name, shield: shield.name }),
+            );
+        }
+
+        const shieldFlavor =
+            attribute === 'attributes.shield' && shield?.isBroken === false
+                ? game.i18n.format('PF2E.UI.applyDamage.shieldActive', { shield: shield.name })
+                : game.i18n.localize('PF2E.UI.applyDamage.shieldInActive');
+        const shieldDamage =
+            attribute === 'attributes.shield' && shield?.isBroken === false && value > 0
+                ? `(${Math.max(0, value - shield.hardness)})`
+                : '';
+        const appliedResult =
+            value > 0
+                ? game.i18n.localize('PF2E.UI.applyDamage.damaged') + value + shieldDamage
+                : game.i18n.localize('PF2E.UI.applyDamage.healed') + value * -1;
+        const modifiedByGM = modifier !== 0 ? `Modified by GM: ${modifier < 0 ? '-' : '+'}${modifier}` : '';
+        const by = game.i18n.localize('PF2E.UI.applyDamage.by');
+        const hitpoints = game.i18n.localize('PF2E.HitPointsHeader').toLowerCase();
+        const message = await renderTemplate('systems/pf2e/templates/chat/damage/result-message.html', {
+            flavorText,
+            by,
+            messageSender,
+            modifiedByGM,
+            actor: token.name,
+            shieldFlavor,
+            appliedResult,
+            hitpoints,
+        });
+        actor.modifyTokenAttribute(attribute, value * -1, true, true, shield).then(() => {
+            const data = {
+                user: game.user._id,
+                speaker: { alias: token.name },
+                content: message,
+                type: CONST.CHAT_MESSAGE_TYPES.EMOTE,
+            };
+            if (game.settings.get('pf2e', 'metagame.secretDamage') && !token?.actor?.hasPlayerOwner) {
+                data.whisper = ChatMessage.getWhisperRecipients('GM');
+            }
+            ChatMessage.create(data);
+        });
+    }
+    return true;
+};
+
+function TLshiftModifyDamage(html, tokenID, multiplier, attributePassed = 'attributes.hp') {
+    new Dialog({
+        title: game.i18n.localize('PF2E.UI.shiftModifyDamageTitle'),
+        content: `<form>
+                <div class="form-group">
+                    <label>${game.i18n.localize('PF2E.UI.shiftModifyDamageLabel')}</label>
+                    <input type="number" name="modifier" value="" placeholder="0">
+                </div>
+                </form>
+                <script type="text/javascript">
+                $(function () {
+                    $(".form-group input").focus();
+                });
+                </script>`,
+        buttons: {
+            ok: {
+                label: 'Ok',
+                callback: async (dialogHtml) => {
+                    let modifier = parseFloat(dialogHtml.find('[name="modifier"]').val());
+                    if (Number.isNaN(modifier)) {
+                        modifier = 0;
+                    }
+                    if (modifier !== undefined) {
+                        await applyActorDamage(html, tokenID, multiplier, attribute, modifier);
+                    }
+                },
+            },
+        },
+        default: 'ok',
+        close: () => {},
+    }).render(true);
+};
+
+function applyTokenDamage(html, tokenID, multiplier, promptModifier = false) {
+    let attribute = 'attributes.hp';
+    const $button = html.find('button.shield-block');
+    if (CONFIG.PF2E.chatDamageButtonShieldToggle && multiplier > 0) {
+        attribute = 'attributes.shield';
+        $button.removeClass('shield-activated');
+        CONFIG.PF2E.chatDamageButtonShieldToggle = false;
+    }
+    const shieldID = $button.attr('data-shield-id') ?? undefined;
+
+    if (promptModifier) {
+        TLshiftModifyDamage(html, tokenID, multiplier, attribute);
+    } else {
+        applyActorDamage(html, tokenID, multiplier, attribute, 0, { shieldID: shieldID });
+    }
+};
+
+
+Hooks.on("renderChatMessage", async (message, html, data) => {
     //if chat message has damage roll
     var _a;
     const damageRoll = message.getFlag("pf2e", "damageRoll")
@@ -10,165 +134,55 @@ Hooks.on('renderChatMessage', async (message, html, data) => {
     if (!isRoll || isD20 || fromRollTable)
         return;
     
-    const $targets = $(await renderTemplate("modules/target-lock/templates/target-list.html", Array.from(data.author.targets)));
-    html.append($targets);
+    // const $targets = $(await renderTemplate("modules/target-lock/templates/target-list.html", Array.from(data.author.targets)));
+    // html.append($targets);
+
+    if (Array.from(data.author.targets).length === 0) {
+        return;
+    }     
+    // else {
+    //     html.append("<hr class='target-lock'/><span class='target-lock-title'>Targets:</span>");
+    // }
+    
+    
+    //TODO --figure out how wrap everything in a div
+    html.append("<div class='target-lock-chat-window'><hr class='target-lock'/></div>")
 
     //foreach target
-        //render Buttons
-        //add callbacks
+    for (const target of Array.from(data.author.targets)) { //cant use foreach due to async
+        const innerHTML = $(await renderTemplate("systems/pf2e/templates/chat/damage/buttons.html", {}));
+        const full = innerHTML.find('button.full-damage');
+        const half = innerHTML.find('button.half-damage');
+        const double = innerHTML.find('button.double-damage');
+        const heal = innerHTML.find('button.heal-damage');
 
+        full.on('click', (event) => {
+            console.log("target-lock | Full Damage");
+            applyTokenDamage(html, target.data._id, 1, event.shiftKey);
+            //applyDamage(innerHTML, 1, event.shiftKey);
+        });
 
-//     const $targetsUI = Array.from(data.author.targets).map(element => {
-//         //duplicate buttons for each target
-//         var innerHTML = $(await renderTemplate("systems/pf2e/templates/chat/damage/buttons.html", Array.from(data.author.targets)));
-//         return innerHTML;
-//     });
+        half.on('click', (event) => {
+            console.log("target-lock | Half Damage");
+            applyTokenDamage(html, target.data._id, 0.5, event.shiftKey);
+            //applyDamage(innerHTML, 1, event.shiftKey);
+        });
 
-//     const $targets = $(await renderTemplate("modules/target-lock/templates/target-list.html", Array.from($targetsUI)));
-//     html.append($targets)
-   
+        double.on('click', (event) => {
+            console.log("target-lock | Double Damage");
+            applyTokenDamage(html, target.data._id, 2, event.shiftKey);
+            //applyDamage(innerHTML, 1, event.shiftKey);
+        });
 
+        heal.on('click', (event) => {
+            console.log("target-lock | Heal Damage");
+            applyTokenDamage(html, target.data._id, -1, event.shiftKey);
+            //applyDamage(innerHTML, 1, event.shiftKey);
+        });
+
+        html.find('div.target-lock-chat-window').append("<span class='target-lock-target-name flavor-text'><b>Target: " + target.data.name +  "</b></span>");
+        html.find('div.target-lock-chat-window').append(innerHTML);   
+    };
+    html.find('div.target-lock-chat-window').append("<hr class='target-lock'/>");
+    //html.find('li > div.chat-damage-buttons').prepend("<span class='target-lock-target-name flavor-text'><b>Selected Tokens:</b></span>");
 });
-
-    //     const full = innerHTML.find('button.full-damage');
-    //     const half = innerHTML.find('button.half-damage');
-    //     const double = innerHTML.find('button.double-damage');
-    //     const heal = innerHTML.find('button.heal-damage');
-    //     const contentSelector = `li.chat-message[data-message-id="${message.id}"] div.hover-content`;
-    //     const $shield = innerHTML
-    //         .find('button.shield-block')
-    //         .attr({ 'data-tooltip-content': contentSelector })
-    //         .tooltipster({
-    //             animation: 'fade',
-    //             trigger: 'click',
-    //             arrow: false,
-    //             contentAsHTML: true,
-    //             debug: BUILD_MODE === 'development',
-    //             interactive: true,
-    //             side: ['top'],
-    //             theme: 'crb-hover',
-    //         });
-    //     $shield.tooltipster('disable');
-    //     innerHTML.find('button.shield-block').attr({ title: LocalizePF2e.translations.PF2E.DamageButton.ShieldBlock });
-    //     // Handle button clicks
-    //     full.on('click', (event) => {
-    //         applyDamage(innerHTML, 1, event.shiftKey);
-    //     });
-    
-    //     half.on('click', (event) => {
-    //         applyDamage(innerHTML, 0.5, event.shiftKey);
-    //     });
-    
-    //     double.on('click', (event) => {
-    //         applyDamage(innerHTML, 2, event.shiftKey);
-    //     });
-    
-    // //    // $shield.on('click', async (event) => {
-    //     //     const tokens = canvas.tokens.controlled.filter((token) => token.actor);
-    //     //     if (tokens.length === 0) {
-    //     //         const errorMsg = LocalizePF2e.translations.PF2E.UI.errorTargetToken;
-    //     //         ui.notifications.error(errorMsg);
-    //     //         event.stopPropagation();
-    //     //         return;
-    //     //     }
-    
-    //     //     // If the actor is wielding more than one shield, have the user pick which shield to block for blocking.
-    //     //     const actor = tokens[0].actor;
-    //     //     const heldShields = actor.itemTypes.armor.filter((armor) => armor.isEquipped && armor.isShield);
-    //     //     const nonBrokenShields = heldShields.filter((shield) => !shield.isBroken);
-    //     //     const multipleShields = tokens.length === 1 && nonBrokenShields.length > 1;
-    //     //     const shieldActivated = $shield.hasClass('shield-activated');
-    
-    //     //     if (multipleShields && !shieldActivated) {
-    //     //         $shield.tooltipster('enable');
-    //     //         // Populate the list with the shield options
-    //     //         const $list = $buttons.find('ul.shield-options');
-    //     //         $list.children('li').remove();
-    
-    //     //         const $template = $list.children('template');
-    //     //         for (const shield of nonBrokenShields) {
-    //     //             const $listItem = $($template.innerHTML());
-    //     //             $listItem.children('input.data').val(shield.id);
-    //     //             $listItem.children('span.label').text(shield.name);
-    //     //             const hardnessLabel = LocalizePF2e.translations.PF2E.ShieldHardnessLabel;
-    //     //             $listItem.children('span.tag').text(`${hardnessLabel}: ${shield.hardness}`);
-    
-    //     //             $list.append($listItem);
-    //     //         }
-    //     //         $list.find('li input').on('change', (event) => {
-    //     //             const $input = $(event.currentTarget);
-    //     //             $shield.attr({ 'data-shield-id': $input.val() });
-    //     //             $shield.tooltipster('close').tooltipster('disable');
-    //     //             $shield.addClass('shield-activated');
-    //     //             CONFIG.PF2E.chatDamageButtonShieldToggle = true;
-    //     //         });
-    //     //         $shield.tooltipster('open');
-    //     //         return;
-    //     //     } else {
-    //     //         $shield.tooltipster('disable');
-    //     //         $shield.removeAttr('data-shield-id');
-    //     //         event.stopPropagation();
-    //     //     }
-    
-    //     //     $shield.toggleClass('shield-activated');
-    //     //     CONFIG.PF2E.chatDamageButtonShieldToggle = !CONFIG.PF2E.chatDamageButtonShieldToggle;
-    //     // });
-    
-    //     heal.on('click', (event) => {
-    //         applyDamage(innerHTML, -1, event.shiftKey);
-    //     });
-
-    //     return innerHTML;
-    // });
-
-    //const $targets = $(await renderTemplate("modules/target-lock/templates/target-list.html", Array.from($targetsUI)));
-
-
-// function applyDamage(html: JQuery<HTMLElement>, multiplier: number, promptModifier = false) {
-//     let attribute = 'attributes.hp';
-//     const $button = html.find('button.shield-block');
-//     if (CONFIG.PF2E.chatDamageButtonShieldToggle && multiplier > 0) {
-//         attribute = 'attributes.shield';
-//         $button.removeClass('shield-activated');
-//         CONFIG.PF2E.chatDamageButtonShieldToggle = false;
-//     }
-//     const shieldID = $button.attr('data-shield-id') ?? undefined;
-
-//     if (promptModifier) {
-//         shiftModifyDamage(html, multiplier, attribute);
-//     } else {
-//         ActorPF2e.applyDamage(html, multiplier, attribute, 0, { shieldID: shieldID });
-//     }
-// }
-
-// function shiftModifyDamage(html: JQuery<HTMLElement>, multiplier: number, attributePassed = 'attributes.hp') {
-//     new Dialog({
-//         title: game.i18n.localize('PF2E.UI.shiftModifyDamageTitle'),
-//         content: `<form>
-//                 <div class="form-group">
-//                     <label>${game.i18n.localize('PF2E.UI.shiftModifyDamageLabel')}</label>
-//                     <input type="number" name="modifier" value="" placeholder="0">
-//                 </div>
-//                 </form>
-//                 <script type="text/javascript">
-//                 $(function () {
-//                     $(".form-group input").focus();
-//                 });
-//                 </script>`,
-//         buttons: {
-//             ok: {
-//                 label: 'Ok',
-//                 callback: async (dialogHtml: JQuery) => {
-//                     let modifier = parseFloat(<string>dialogHtml.find('[name="modifier"]').val());
-//                     if (Number.isNaN(modifier)) {
-//                         modifier = 0;
-//                     }
-//                     if (modifier !== undefined) {
-//                         await ActorPF2e.applyDamage(html, multiplier, attributePassed, modifier);
-//                     }
-//                 },
-//             },
-//         },
-//         default: 'ok',
-//         close: () => {},
-//     }).render(true);
